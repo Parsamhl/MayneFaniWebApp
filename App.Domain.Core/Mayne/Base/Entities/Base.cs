4dde9c106be94572daf7323c5a636f8469716c2b
parsa <mparsah$@gmail.com>
2025-03-23 16:44:47 +0330

    Entities Created

---
+++ App.Domain.Core/Mayne/Base/Entities/Base.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.Mayne.Base.Entities
+{
+    public class Base
+    {
+
+        public int BaseId { get; set; }
+        public string BaseName { get; set; }
+        public int BaseNumber { get; set; }
+
+        
+    }
+}
