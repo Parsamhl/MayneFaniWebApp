43e39c365111a4626c7527a2983246d1969fec03
parsa <mparsah$@gmail.com>
2025-03-26 10:56:13 +0330

    DataBase Ef  Added

---
+++ App.Infra.Db.SqlServer/MayneDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Infra.Db.SqlServer
+{
+    public class MayneDbContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+        }
+
+    }
+}
