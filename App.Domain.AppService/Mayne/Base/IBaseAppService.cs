91470d8733a4b3e98647ad3bf4b0526116d99e59
parsa <mparsah$@gmail.com>
2025-04-30 19:19:14 +0330

    AppService Interface

---
+++ App.Domain.AppService/Mayne/Base/IBaseAppService.cs
@@ -0,0 +1,15 @@
+﻿using App.Domain.Core.Mayne.Result;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.AppService.Mayne.NewFolder
+{
+    public interface IBaseAppService
+    {
+        Task<Result> IsActive(int BaseNumber);
+        Task <List<BaseDto>>
+    }
+}
---
+++ App.Domain.AppService/Mayne/Car/ICarAppService.cs
@@ -0,0 +1,15 @@
+﻿using App.Domain.Core.Mayne.Cars.Dtos;
+using App.Domain.Core.Mayne.Result;
+
+
+namespace App.Domain.AppService.Mayne.Car
+{
+    public interface ICarAppService
+    {
+        Task<Result> AddCar(CarDto carDto, CancellationToken cancellation);
+        Task<Result> DeleteCar(int id, CancellationToken cancellation);
+        Task<CarDto> GetCar(int id, CancellationToken cancellation);
+        Task<List<CarDto>> GetCars(CancellationToken cancellation);
+        Task<Result> UpdateCar(CarDto carDto, CancellationToken cancellation);
+    }
+}
