8699d477e6ee5b880eaa81b63a1e76840bf46d84
parsa mousavi <mparsah4@gmail.com>
2025-05-11 23:29:54 +0330

    Admin Area Update

---
+++ MayneFaniWebApp(MVC)/Areas/Admin/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MayneFaniWebApp_MVC_.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
